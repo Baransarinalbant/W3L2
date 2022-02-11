89815bdce21231ca3b37c89f12e14c86009fa49a
Baran Sarinalbant <baransarinalbant@hotmail.com>
2022-02-11 18:11:02 +0100

    klar

--- js/script.js
+++ js/script.js
@@ -22,6 +22,7 @@ function selectSubject() {
 	this.selectedIndex = 0; // Återställ menyn
 } // End selectSubject
 
+// Söker efter information från PHP filen 
 function requestData(subject) {
 	let request = new XMLHttpRequest(); // Object för Ajax-anropet
 	request.open("GET","getSubInfo.php?file=https://medieteknik.lnu.se/1me323/subjects.xml&id=" + subject,true);
@@ -34,18 +35,15 @@ function requestData(subject) {
 	};
 } // End requestDepartmentinfo
 
+// Hämtar informationen och lägger in det på sidan
 function getData(XMLcode) {
-	let subjectElems = XMLcode.getElementsByTagName("subject"); // Lista (array) med alla subject-element
 	let HTMLcode = ""; // Textsträng med ny HTML-kod som skapas
-	for (let i = 0; i < subjectElems.length; i++) {
-		let nameElem = subjectElems[i].getElementsByTagName("name")[0];
-		let infoElem = subjectElems[i].getElementsByTagName("info")[0];
+		let nameElem = XMLcode.getElementsByTagName("name")[0];
+		let infoElem = XMLcode.getElementsByTagName("info")[0];
 		HTMLcode += "<h3>" + nameElem.firstChild.data  + "</h3>";
 		HTMLcode += infoElem.firstChild.data + "</p>";
-	}
-	subjectInfoElem.innerHTML = HTMLcode;
+	subjectInfoElem.innerHTML = HTMLcode;	// Skriver ut texten i rätt plats
 } // End getData
-
 
 
 
@@ -53,14 +51,15 @@ function getData(XMLcode) {
 
 // Avläs menyn för val av ämne för kurser
 function selectCourses() {
-	let course = this.value; // Ämne i menyns option-element
+	let course = this.selectedIndex; // Ämne i menyns option-element
 	requestInfo(course);
 	this.selectedIndex = 0;
 } // End selectCourses
 
-function requestInfo(value) {
+// Söker efter information från xml filerna
+function requestInfo(course) {
 	let request = new XMLHttpRequest(); // Object för Ajax-anropet
-	request.open("GET","xml/" + value + ".xml",true);
+	request.open("GET","xml/courselist" + course + ".xml",true);
 	request.send(null); // Skicka begäran till servern
 	request.onreadystatechange = function () { // Funktion för att avläsa status i kommunikationen
 		if (request.readyState == 4) // readyState 4 --> kommunikationen är klar
@@ -70,9 +69,10 @@ function requestInfo(value) {
 	};
 } // End requestDepartmentinfo
 
+// Hämtar informationen och lägger in det på sidan i andra menyn
 function getInfo(XMLcode) {
 	let subElem = XMLcode.getElementsByTagName("courselist");
-	let subjectElems = XMLcode.getElementsByTagName("course"); // Lista (array) med alla subject-element
+	let subjectElems = XMLcode.getElementsByTagName("course"); // Lista (array) med alla course-element
 	let HTMLcode = ""; // Textsträng med ny HTML-kod som skapas
 	for (let i = 0; i < subElem.length; i++) {
 	let headerElem = subElem[i].getElementsByTagName("subject")[0];
